2f369e623a5677faf8c026f9d63405f19b232d89
jackson <jacksonsdss@gmail.com>
2020-03-13 16:53:48 -0300

    Atualização do sql

--- ProjetoWeb/src/TestaInsercaoComParametro.java
+++ ProjetoWeb/src/TestaInsercaoComParametro.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -8,19 +9,25 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args)throws SQLException {
 		
-		String nome ="Mouse";
+		int id = 9;
+		String nome ="Teste";
 		String descricao="Mouse sem fio";
 		
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
 		
-		Statement stm = connection.createStatement();
-		stm.execute("INSERT INTO PRODUTO(id, nome, descricao) VALUES ('6','" + nome +"','"+ descricao + "')", Statement.RETURN_GENERATED_KEYS);
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO(id, nome, descricao) VALUES (?,?,?)", Statement.RETURN_GENERATED_KEYS);
+		
+		stm.setInt(1, id);
+		stm.setString(2, nome);
+		stm.setString(3, descricao);
+		
+		stm.execute();
 		
 		ResultSet rst = stm.getGeneratedKeys();
 		while(rst.next()){
-			Integer id = rst.getInt(1);
-			System.out.println("o id criado foi" + id);
+			Integer id1 = rst.getInt(1);
+			System.out.println("o id criado foi" + id1);
 	}
 }	
 }
